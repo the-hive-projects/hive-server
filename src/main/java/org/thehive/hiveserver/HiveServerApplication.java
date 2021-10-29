13b57dd28ef8cf83dd9d7bbc7af13ba2721afc85
SemihBG <buyukgungorsemih@gmail.com>
2021-10-29 22:23:44 +0300

    adds required web and data dependencies, creates config files and adds simple properties

--- src/main/java/org/thehive/hiveserver/HiveServerApplication.java
+++ src/main/java/org/thehive/hiveserver/HiveServerApplication.java
@@ -3,7 +3,7 @@ package org.thehive.hiveserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(proxyBeanMethods = false)
 public class HiveServerApplication {
 
     public static void main(String[] args) {
