59be5c4e86ede9b49296a673ec1f0f1a5eeb44f7
SemihBG <buyukgungorsemih@gmail.com>
2021-10-31 14:48:52 +0300

    adjustment of user api url endpoint authentication security, creates and adds custom authentication entry point to return standardized exception response

--- src/main/java/org/thehive/hiveserver/config/WebSecurityConfig.java
+++ src/main/java/org/thehive/hiveserver/config/WebSecurityConfig.java
@@ -1,12 +1,22 @@
 package org.thehive.hiveserver.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.thehive.hiveserver.error.ExceptionResponse;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @RequiredArgsConstructor
 @EnableWebSecurity
@@ -14,13 +24,17 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final PasswordEncoder passwordEncoder;
     private final UserDetailsService userDetailsService;
+    private final ObjectMapper objectMapper;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .httpBasic()
+                .authenticationEntryPoint(new ExceptionResponseAuthenticationEntryPoint(objectMapper))
                 .and()
                 .authorizeRequests()
+                .antMatchers(HttpMethod.POST, "/user")
+                .permitAll()
                 .anyRequest()
                 .authenticated()
                 .and()
@@ -32,6 +46,21 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userDetailsService)
                 .passwordEncoder(passwordEncoder);
+    }
+
+    @RequiredArgsConstructor
+    public static class ExceptionResponseAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+        private final ObjectMapper objectMapper;
+
+        @Override
+        public void commence(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, AuthenticationException e) throws IOException {
+            httpServletResponse.setStatus(HttpStatus.UNAUTHORIZED.value());
+            var exceptionResponse = ExceptionResponse.of(e, httpServletRequest, HttpStatus.UNAUTHORIZED);
+            httpServletResponse.setContentType("application/json");
+            objectMapper.writeValue(httpServletResponse.getWriter(), exceptionResponse);
+        }
+
     }
 
 }
--- src/main/java/org/thehive/hiveserver/error/ExceptionResponse.java
+++ src/main/java/org/thehive/hiveserver/error/ExceptionResponse.java
@@ -1,14 +1,17 @@
 package org.thehive.hiveserver.error;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class ExceptionResponse {
 
     private long timestamp;
@@ -18,5 +21,23 @@ public class ExceptionResponse {
     private String message;
     private String path;
     private String method;
+
+    public static ExceptionResponse of(@NonNull Throwable throwable, @NonNull HttpServletRequest request) {
+        return throwable instanceof ResponseStatusException ?
+                of(throwable, request, ((ResponseStatusException) throwable).getStatus()) :
+                of(throwable, request, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    public static ExceptionResponse of(@NonNull Throwable throwable, @NonNull HttpServletRequest request, @NonNull HttpStatus status) {
+        return ExceptionResponse.builder()
+                .timestamp(System.currentTimeMillis())
+                .status(status.value())
+                .error(status.getReasonPhrase())
+                .exception(throwable.getClass().getName())
+                .message(throwable.getMessage())
+                .path(request.getServletPath())
+                .method(request.getMethod())
+                .build();
+    }
 
 }
